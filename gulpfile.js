2b7fe282be2d72fc6b8b7229727676c7c830a53b
Godicks <matvej.novik@gmail.com>
2020-04-26 19:34:05 +0300

    made code and style for filterMob

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,14 @@ var gulp = require('gulp'), // Подключаем Gulp
     rename = require("gulp-rename") // Переименование файлов
     browserSync = require('browser-sync');// Создаю сервер на браузере
 
-    gulp.task("html", function() {
+gulp.task("html", function() {
     return gulp.src("src/*.html")
     .pipe(gulp.dest("dist"));
+});
+
+gulp.task("html-allRooms", function() {
+    return gulp.src("src/allRooms/*.html")
+    .pipe(gulp.dest("dist/allRooms"));
 });
 
 gulp.task('browserSync', function() {
@@ -26,6 +31,10 @@ gulp.task('browserSync', function() {
   gulp.watch("dist/css/*.css").on('change', browserSync.reload);
   gulp.watch("dist/images/*.+(jpg|jpeg|png|gif|svg)").on('change', browserSync.reload);
   gulp.watch("dist/js/*.js").on('change', browserSync.reload);
+/*  gulp.watch("dist/allRooms/*.html").on('change', browserSync.reload);
+  gulp.watch("dist/allRooms/css/*.css").on('change', browserSync.reload);
+  gulp.watch("dist/allRooms/images/*.+(jpg|jpeg|png|gif|svg)").on('change', browserSync.reload);
+  gulp.watch("dist/allRooms/js/*.js").on('change', browserSync.reload); */ 
 });
 
 
@@ -42,12 +51,33 @@ gulp.task('sass', function() { // Создаем таск "sass"
     .pipe(browserSync.stream());
   });
 
+gulp.task('sass-allRooms', function() { // Создаем таск "sass"
+  return gulp.src(['src/allRooms/scss/**/*.scss']) // Берем источник
+    .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+    .pipe(autoprefixer({
+       browsers: ['last 2 versions'],
+       cascade: false
+     }))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/allRooms/css')) // Выгружаем результата в папку css
+    .pipe(browserSync.stream());
+  });
+
 gulp.task("scripts", function() {
     return gulp.src("src/js/*.js") // директория откуда брать исходники
         .pipe(concat('scripts.js')) // объеденим все js-файлы в один 
         .pipe(uglify()) // вызов плагина uglify - сжатие кода
         .pipe(rename({ suffix: '.min' })) // вызов плагина rename - переименование файла с приставкой .min
         .pipe(gulp.dest("dist/js")); // директория продакшена, т.е. куда сложить готовый файл
+});
+
+gulp.task("scripts-allRooms", function() {
+    return gulp.src("src/allRooms/js/*.js") // директория откуда брать исходники
+        .pipe(concat('scripts.js')) // объеденим все js-файлы в один 
+        .pipe(uglify()) // вызов плагина uglify - сжатие кода
+        .pipe(rename({ suffix: '.min' })) // вызов плагина rename - переименование файла с приставкой .min
+        .pipe(gulp.dest("dist/allRooms/js")); // директория продакшена, т.е. куда сложить готовый файл
 });
 
 gulp.task('imgs', function() {
@@ -60,6 +90,16 @@ gulp.task('imgs', function() {
         .pipe(gulp.dest("dist/images"))
 });
 
+gulp.task('imgs-allRooms', function() {
+    return gulp.src("src/allRooms/images/**/*.+(jpg|jpeg|png|gif|svg|webp)")
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true
+        }))
+        .pipe(gulp.dest("dist/allRooms/images"))
+});
+
 gulp.task('watch', function() {
     gulp.watch('src/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch("src/*.html", gulp.parallel('html'));
@@ -67,4 +107,11 @@ gulp.task('watch', function() {
     gulp.watch("src/images/*.+(jpg|jpeg|png|gif|svg)", gulp.parallel('imgs'));
 });
 
-gulp.task("default", gulp.parallel("html", "sass", "scripts", "imgs","browserSync", "watch"));
+gulp.task('watch-allRooms', function() {
+    gulp.watch('src/allRooms/scss/**/*.scss', gulp.parallel('sass-allRooms'));
+    gulp.watch("src/allRooms/*.html", gulp.parallel('html-allRooms'));
+    gulp.watch("src/allRooms/js/*.js", gulp.parallel('scripts-allRooms'));
+    gulp.watch("src/allRooms/images/*.+(jpg|jpeg|png|gif|svg)", gulp.parallel('imgs-allRooms'));
+});
+
+gulp.task("default", gulp.parallel("html", "sass", "scripts", "imgs","browserSync", "watch","html-allRooms", "sass-allRooms", "scripts-allRooms", "imgs-allRooms", "watch-allRooms"));
